4daec31f8f09a537f56a7a82e4d690c940876000
amandaxfung <39556956+amandaxfung@users.noreply.github.com>
2018-06-06 19:58:40 -0400

    Update hw1.js

--- hw1.js
+++ hw1.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const request= require('request');
+
+const request = require('request');
 
 
-router.get('/inputstring', function(req,res,next) {
-    let str = req.params.input_string;
+router.get('/:instring', function(req,res,next) {
+    let str = req.params.instring;
     let len = str.length;
-    res.json({string:str, length:len});
+    res.json('{"string:" + str, "length:" + len }');
 });
 
 
 router.post('/', function(req,res,next) {
-    let str = req.body.input_string;
+    let str = req.body.string;
     let len = str.length;
-    res.json({string:str,length:len});
+    res.send('{"string:" + str, "length:" + len }');
 });
 
 module.exports = router;
