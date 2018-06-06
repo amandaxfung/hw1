ed7a7feedf221e3fc35a7fc8ca6836c41a01aa70
amandaxfung <39556956+amandaxfung@users.noreply.github.com>
2018-06-05 21:36:03 -0400

    Update and rename hw1.js to hw2.js

---
+++ hw2.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const request= require('request');
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/cs411')
+const db = mongoose.connection
+
+const Schema = mongoose.Schema
+const personSchema = new Schema({
+    string: String,
+    length: float,
+})
+
+//const people = mongoose.model('people', personSchema)
+
+
+router.get('/inputstring', function(req,res,next) {
+    let str = req.params.input_string;
+    let len = str.length;
+    res.json({string:str, length:len});
+});
+
+
+router.post('/db', function(req,res,next) {
+    let str = req.body.input_string;
+    let len = str.length;
+    res.send({string:str,length:len});
+});
+
+module.exports = router;
